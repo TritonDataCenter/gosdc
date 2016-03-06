bbfea6cbf002789fbbbd512b409accb6f24c11fe
Florin STAN <namtzigla@users.noreply.github.com>
2016-03-05 23:14:17 -0700

    added services to api url parts

--- cloudapi/cloudapi.go
+++ cloudapi/cloudapi.go
@@ -46,6 +46,7 @@ const (
 	apiFabricVLANs             = "fabrics/default/vlans"
 	apiFabricNetworks          = "networks"
 	apiNICs                    = "nics"
+	apiServices                = "services"
 
 	// CloudAPI actions
 	actionExport    = "export"
